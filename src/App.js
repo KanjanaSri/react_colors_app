3afd7a366ab92ee5a50dfdefca8d398781b5958c
SKarnjana <ksrisumrarn.pam@gmail.com>
2023-06-16 14:45:38 -0700

    add slider and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Palette from './Palette';
 import seedPalettes from './seedPalettes';
+import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
     <div className="App">
-      <Palette {...seedPalettes[2]}/>
+      <Palette palette={ generatePalette(seedPalettes[2])} />
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
 import ColorBox from './ColorBox';
+import Navbar from './Navbar';
 import './Palette.css';
 
 export default class Palette extends Component {
+  constructor(props){
+    super(props);
+    this.state = { level: 500, format: 'hex' };
+    this.changeLevel = this.changeLevel.bind(this);
+    this.changeFormat = this.changeFormat.bind(this);
+  }
+
+  changeLevel(newLevel) {
+    this.setState({ level: newLevel });
+  }
+
+  changeFormat(value) {
+    this.setState({ format: value });
+  }
+ 
   render() {
-    const colorBoxes = this.props.colors.map( color => (
-      <ColorBox background={color.color} name={color.name} />
+    const { colors, paletteName, emoji } = this.props.palette;
+    const { level, format }  = this.state;
+    const colorBoxes = colors[level].map( color => (
+      <ColorBox background={color[format]} name={color.name} key={color.id} />
     ))
     return (
       <div className='Palette'>
-        {/* Navbar goes here */}
-        <div className='Palette-colors'>
-          {colorBoxes}
-          {/* footer eventually */}
-        </div>
+        <Navbar 
+          level={level} 
+          changeLevel={this.changeLevel}
+          handleChange={this.changeFormat}
+        />
+        <div className='Palette-colors'>{colorBoxes}</div>
+        <footer className='Palette-footer'>
+          {paletteName}
+          <span className='emoji'>{emoji}</span>
+        </footer>
       </div>
     )
   }
